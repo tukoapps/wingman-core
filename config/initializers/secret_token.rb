e256ea532b074fd2db76485d7bedd5c2de331426
TheZallen <zak.allen@sbcglobal.net>
2014-06-22 13:58:01 -0500

    initial commit with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wingman::Application.config.secret_key_base = '5c2ced87099d2a4785bf7abeab2703319c9d2db2cf6f1fc2c1912d553c567dc792b964e5dcf80551586f7386570a2a1e35b3736a2d0d885a19895812148cfab5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wingman::Application.config.session_store :cookie_store, key: '_wingman_session'
