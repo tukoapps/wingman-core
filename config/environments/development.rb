69d2a778bccafc62db9a10316c3499439f499ee3
TheZallen <zak.allen@sbcglobal.net>
2014-06-28 22:18:13 -0500

    fingers crossed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,6 @@ Wingman::Application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
 end
