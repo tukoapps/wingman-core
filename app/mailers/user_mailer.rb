340b0f4fbfde3feb98ccdd5690be2cd77df0faf7
TheZallen <zak.allen@sbcglobal.net>
2014-06-29 22:16:47 -0500

    new email sender

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ActionMailer::Base
-  default from: 'no-reply@get-wingman.com'
+  default from: "\"WingMan\" <suggestions@get-wingman.com>"
  
   def welcome_email(email)
     @email = email
-    mail(to: @email, subject: 'Welcome to WingMan')
+    mail(to: @email, subject: 'Welcome to WingMan!')
   end
 end
