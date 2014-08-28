0e0cd24c86f35da72bb990a1251d55aeb7668c45
TheZallen <zak.allen1009@gmail.com>
2014-08-28 16:30:17 -0500

    delete assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Wingman::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.precompile += ['home.css', 'custom.css']
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
